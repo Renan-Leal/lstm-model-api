3365f3c504c3cdc20ea9317a2394b4593916ba45
renan <renan.leallol@gmail.com>
2026-02-11 00:21:57 -0300

    config:add project setup scripts for Docker and environment

---
+++ scripts/run-prod.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Caminho absoluto do diretório do script
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+
+# Caminho absoluto para a pasta config
+CONFIG_DIR="$SCRIPT_DIR/../config"
+
+docker compose \
+  -f "$CONFIG_DIR/docker-compose-prod.yml" \
+  -f "$CONFIG_DIR/docker-compose.dozzle.yml" \
+  up -d
